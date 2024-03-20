3818f8b771a83c4f7e3218c960c63eef1a9e0c93
aman singh <amaninfo201415@gmail.com>
2024-03-20 09:44:28 +0530

    added

--- social-media-app/src/Component/Middlecomp.jsx
+++ social-media-app/src/Component/Middlecomp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import snap from "../assets/snap.png";
 export const Middlecomp = () => {
   return (
-    <div className="mr-[30px] bg-[#f0f8ff]">
+    <div className="mr-[30px] ">
       <img src={snap} className="h-[600px] w-[700px] mt-22" />
     </div>
   );
